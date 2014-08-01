54833e7820b94b7b4841c13d0d774566bb7ba0c3
PythonNut <PythonNut@users.noreply.github.com>
2014-08-01 03:36:00 +0000

    Added title and tab title module

---
+++ modules/19_title.zsh
@@ -0,0 +1,54 @@
+# set the title
+function _setTitle() {
+  local titlestart titlefinish
+
+  # determine the terminals escapes
+  case "$_OLD_TERM" in
+    aixterm|dtterm|putty|rxvt|xterm*)
+      titlestart='\033]0;'
+      titlefinish='\007';;
+    cygwin)
+      titlestart='\033];'
+      titlefinish='\007';;
+    konsole)
+      titlestart='\033]30;'
+      titlefinish='\007';;
+    screen*|screen)
+      titlestart='\033k'
+      titlefinish='\033\';;
+    *)
+      if type tput >/dev/null 2>&1; then
+        if tput longname >/dev/null 2>&1; then
+          titlestart="$(tput tsl)"
+          titlefinish="$(tput fsl)"
+        fi
+      else
+        titlestart=''
+        titlefinish=''
+      fi
+  esac
+
+  test -z "${titlestart}" && return 0
+  [[ -n $EMACS || $TERM == "dumb" ]] && return 0
+  if [[ $EUID == 0 ]]; then
+    printf "${titlestart}$* ! ${cur_command}${titlefinish}"
+  else
+    printf "${titlestart}$* ${cur_command}${titlefinish}"
+  fi
+}
+
+# if title set manually, dont set automatically
+function settitle() {
+  _titleManual=1
+  _setTitle $1
+  if [[ ! -n $1 ]]; then
+    _titleManual=0
+    chpwd_force
+  fi
+}
+
+{
+  # let's initialize the title
+  alias settitle="nocorrect settitle"
+  _setTitle $(minify_path .)
+} &>> ~/.zsh.d/startup.log
