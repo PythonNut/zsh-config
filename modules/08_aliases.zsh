28e48b9b98145ba8450a34b657de66f68a6fa082
PythonNut <PythonNut@users.noreply.github.com>
2014-08-04 00:10:10 +0000

    Switched to single quotes in alias declarations

--- modules/08_aliases.zsh
+++ modules/08_aliases.zsh
@@ -3,54 +3,54 @@
 # =======
 {
   # proxy aliases
-  BORING_FILES="*\~|*.elc|*.pyc|!*|_*|*.swp"
-  alias lsa="\ls --color --group-directories-first"
-  alias lst="lsa -I \"${BORING_FILES:gs/\|/\" -I \"/}\""
-  alias egrep="nocorrect noglob \egrep --line-buffered --color=auto"
-  
+  BORING_FILES='*\~|*.elc|*.pyc|!*|_*|*.swp'
+  alias lsa='\ls --color --group-directories-first'
+  alias lst='lsa -I "${BORING_FILES:gs/\|/" -I "/}"'
+  alias egrep='nocorrect noglob \egrep --line-buffered --color=auto'
+
   # ls aliases
-  alias ls="lst -BFx"
+  alias ls='lst -BFx'
   alias l='lst -lFBGh'
   alias ll='lsa -lAFGh'
-  alias lss="lst -BFshx"
-  alias lsp="\ls"
-  
+  alias lss='lst -BFshx'
+  alias lsp='\ls'
+
   # saftey aliases
-  alias rm="rm -i" cp="cp -i"
-  alias rmf="\rm" cpf="\cp"
-  
+  alias rm='rm -i' cp='cp -i'
+  alias rmf='\rm' cpf='\cp'
+
   # global aliases
-  alias -g G="|& egrep -i"
-  alias -g L="|& less -R"
-  alias -g Lr="|& less"
-  alias -g D=">&/dev/null"
-  alias -g W="|& wc -l -c"
-  alias -g Q=">&/dev/null&"
-  alias -g ,,=";=read -n1 -rp 'Press any key to continue...'"
-  
+  alias -g G='|& egrep -i'
+  alias -g L='|& less -R'
+  alias -g Lr='|& less'
+  alias -g D='>&/dev/null'
+  alias -g W='|& wc -l -c'
+  alias -g Q='>&/dev/null&'
+  alias -g ,,=';=read -n1 -rp 'Press any key to continue...''
+
   # regular aliases
-  alias su="su -"          
-  alias watch="\watch -n 1 -d "    
-  alias emacs="\emacs -nw"       
-  alias df='\df -h'          
-  alias ping="\ping -c 10"       
-  alias exi="exit"           
-  alias locate="\locate -ib"     
-  alias exit=" exit"         
-  alias tail='\tail -n $(tput lines)'
-  alias y="yaourt"
-  alias yi="yaourt -Sa"
-  alias yu="yaourt -Syyua --noconfirm"
-  alias yuu="yaourt --sucre"
-  
+  alias su='su -'
+  alias watch='\watch -n 1 -d '
+  alias emacs='\emacs -nw'
+  alias df='\df -h'
+  alias ping='\ping -c 10'
+  alias exi='exit'
+  alias locate='\locate -ib'
+  alias exit=' exit'
+  alias tail="\tail -n $(tput lines)"
+  alias y='yaourt'
+  alias yi='yaourt -Sa'
+  alias yu='yaourt -Syyua --noconfirm'
+  alias yuu='yaourt --sucre'
+
   # suppression aliases
-  alias yum="nocorrect noglob \yum"  
-  alias man="nocorrect noglob \man"  
-  alias find="noglob find"       
-  alias touch="nocorrect \touch"   
-  alias mkdir="nocorrect \mkdir"   
-  alias killall="nocorrect \killall" 
-  alias yum-config-manager="nocorrect noglob \yum-config-manager"
+  alias yum='nocorrect noglob \yum'
+  alias man='nocorrect noglob \man'
+  alias find='noglob find'
+  alias touch='nocorrect \touch'
+  alias mkdir='nocorrect \mkdir'
+  alias killall='nocorrect \killall'
+  alias yum-config-manager='nocorrect noglob \yum-config-manager'
 } always {
   unfunction a
 } &>> ~/.zsh.d/startup.log
