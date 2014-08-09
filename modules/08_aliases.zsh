438c63d5222a8cc0df64c21abd131379ff08dd11
PythonNut <PythonNut@users.noreply.github.com>
2014-08-09 04:18:03 +0000

    Added git fork dynamic alias

--- modules/08_aliases.zsh
+++ modules/08_aliases.zsh
@@ -94,6 +94,8 @@ function alias () {
   alias gca!='git commit -v -a --amend'
 
   alias gck='git checkout'
+  alias -ec gfork='echo git checkout -b {} ${${(f)vcs_raw_data}[4]}'
+  
   alias gb='git branch'
   alias gm='git merge -X patience'
   alias gr="git rebase -X patience"
