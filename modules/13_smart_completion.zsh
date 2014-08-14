a0bc8ab8325709e12486b79296ef932a4a8701e8
PythonNut <PythonNut@users.noreply.github.com>
2014-08-14 10:21:07 -0700

    Made word extraction more efficient in pcomplete

--- modules/13_smart_completion.zsh
+++ modules/13_smart_completion.zsh
@@ -75,9 +75,9 @@ function pcomplete() {
 
       # detect multiple auto-fu matches
       for i in $region_highlight; do
-        i=("${(@s/ /)i}")
+        i=("${(@)${(@s/ /)i}%% *}")
         _setTitle $i
-        if [[ $i[3] == *black* ]] && ((${${i[2]%% *}:-0} - ${${i[1]%% *}:-0} > 0 && ${${i[1]%% *}:-0} > 1)); then
+        if [[ $i[3] == *black* ]] && ((${i[2]:-0} - ${i[1]:-0} > 0 && ${i[1]:-0} > 1)); then
           $0_forward_word
           break
         fi
@@ -85,9 +85,9 @@ function pcomplete() {
 
       # detect single auto-fu match
       for i in $region_highlight; do
-        i=("${(@s/ /)i}")
+        i=("${(@)${(@s/ /)i}%% *}")
         _setTitle $i
-        if [[ $i[3] == *underline* ]] && ((${${i[2]%% *}:-0} - ${${i[1]%% *}:-0} > 0)); then
+        if [[ $i[3] == *underline* ]] && ((${i[2]:-0} - ${i[1]:-0} > 0)); then
           if  [[ $BUFFER != (*/|* */*) ]]; then
             file_match="t"
           fi
