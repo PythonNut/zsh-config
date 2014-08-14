0aae2f80bcf1a01870c9ede66a3d45f044548c68
PythonNut <PythonNut@users.noreply.github.com>
2014-08-13 20:09:30 -0700

    Added -s option to deserialize and improve docs

--- modules/10_async_framework.zsh
+++ modules/10_async_framework.zsh
@@ -1,6 +1,7 @@
 TMPPREFIX=/dev/shm/
 
 function zsh_pickle () {
+  # i - the name of the pickle
   emulate -LR zsh
   zparseopts -D -E i:=ID
   local session_id="zsh.$$.$ID[2]"
@@ -8,22 +9,26 @@ function zsh_pickle () {
 }
 
 function zsh_unpickle () {
-  zparseopts -D -E i:=ID
+  # i - the name of the pickle
+  # s - silently fail if pickle does not exists
   emulate -LR zsh
+  zparseopts -D -E i:=ID s=S
   {
     # force variables to go up scope 
     declare() { builtin declare -g "$@"; }
     typeset() { builtin typeset -g "$@"; }
     
     local session_id="zsh.$$.$ID[2]"
-    source ${TMPPREFIX}$session_id
-    
+    if [[ -f ${TMPPREFIX}$session_id || ! -n "$S" ]]; then
+      source ${TMPPREFIX}$session_id
+    fi
   } always {
     unset -f typeset declare
   }
 }
 
 function zsh_pickle_cleanup () {
+  # i - the name of the pickle (or all pickles)
   emulate -LR zsh
   zparseopts -D -E i:=ID
   if [[ -n "$ID[2]" ]]; then
