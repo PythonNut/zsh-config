d108f6c8cad64e0ed290054bb821a0c8f047c35d
PythonNut <PythonNut@users.noreply.github.com>
2014-08-18 03:42:20 +0000

    Made prediction more "correct"

--- modules/13_smart_completion.zsh
+++ modules/13_smart_completion.zsh
@@ -48,26 +48,25 @@ function pcomplete() {
 
     setopt local_options complete_in_word list_packed list_rows_first
 
-    if [[ $#LBUFFER == 0 || $LBUFFER == $predict_buffer ]]; then
-      # zle dwim
+    if [[ $#LBUFFER == 0 || "$LBUFFER" == "$predict_buffer" ]]; then
       if [[ $#BUFFER == 0 ]]; then
         prediction_index=-1
       fi
       local prediction_array
       prediction_index=$(( $prediction_index + 1 ))
       if [[ $prediction_index == 0 ]]; then
-          zle dwim
-          # catch the catch-all dwim case (temporary)
-          if [[ $BUFFER == sudo* ]]; then
-            prediction_index=$(( $prediction_index + 1 ))
-            prediction_array=("${(@f)$(predict_next_line)}")
-            LBUFFER=$prediction_array[$prediction_index]
-          fi
-      else
-        prediction_array=("${(@f)$(predict_next_line)}")
-        LBUFFER=$prediction_array[$prediction_index]
+        zle dwim
+        zle end-of-line
+        # catch the catch-all dwim case (temporary)
+        if [[ "$BUFFER" == (sudo*) ]]; then
+          prediction_index=$(( $prediction_index + 1 ))
+        else
+          return 0
+        fi
       fi
-      predict_buffer=$LBUFFER
+      prediction_array=("${(@f)$(predict_next_line)}")
+      LBUFFER="$prediction_array[$prediction_index]"
+      predict_buffer="$LBUFFER"
   
       _zsh_highlight
   
