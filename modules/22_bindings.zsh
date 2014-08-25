c2c698ef0eef217c65429d97d975ea06b91c157c
PythonNut <PythonNut@users.noreply.github.com>
2014-08-25 16:53:44 -0700

    Removed current ... = ../.. approximate hack

--- modules/22_bindings.zsh
+++ modules/22_bindings.zsh
@@ -46,25 +46,6 @@ function after-first-word() {
 zle -N after-first-word
 global_bindkey "^X1" after-first-word
 
-# rationalize dots
-rationalise_dot () {
-  emulate -LR zsh
-  # typing .... becomes ../../../ etc.
-  local MATCH # keep the regex match from leaking to the environment
-  if [[ $LBUFFER =~ '(^|/| |      |'$'\n''|\||;|&)\.\.$' ]]; then
-    LBUFFER+=/
-    zle self-insert
-    zle self-insert
-  else
-    zle self-insert
-  fi
-}
-
-zle -N rationalise_dot
-global_bindkey . rationalise_dot
-# without this, typing a "." aborts incremental history search
-bindkey -M isearch . self-insert
-
 # ^Z to foreground the last suspended job.
 foreground-current-job() { fg; }
 zle -N foreground-current-job
