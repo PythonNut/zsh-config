44f0c62af459336f5253c915f446b7e4f940b5ae
PythonNut <PythonNut@users.noreply.github.com>
2014-09-06 17:38:37 -0700

    Added X-clipboard <==> kill-ring syncing

--- modules/09_vim.zsh
+++ modules/09_vim.zsh
@@ -22,3 +22,26 @@ zle -N vi-insert _vi-insert
 source ~/.zsh.d/zsh-vim-pattern-search/en.zsh
 source ~/.zsh.d/zsh-vim-textobjects/opp.zsh
 source ~/.zsh.d/zsh-vim-textobjects/opp/surround.zsh
+
+zsh-x-kill-region () {
+  zle kill-region
+  print -rn $CUTBUFFER | xsel -i
+}
+zsh-x-yank () {
+  CUTBUFFER=$(xsel -o -p </dev/null)
+  zle yank
+}
+
+zsh-x-copy-region-as-kill () {
+  zle copy-region-as-kill
+  print -rn $CUTBUFFER | xsel -i
+}
+
+if (( $+commands[xsel] )); then
+  zle -N zsh-x-yank
+  zle -N zsh-x-kill-region
+  zle -N zsh-x-copy-region-as-kill
+  global_bindkey '\eW' zsh-x-copy-region-as-kill
+  global_bindkey '^W' zsh-x-kill-region
+  global_bindkey '^y' zsh-x-yank
+fi
