6c351423f56fe3b0b22c12e08bc4b7fdb25648b4
PythonNut <PythonNut@users.noreply.github.com>
2014-09-20 16:54:09 +0000

    Made prompt behave nicely in Midnight Commander

--- modules/18_prompt.zsh
+++ modules/18_prompt.zsh
@@ -9,15 +9,34 @@ function compute_prompt () {
   emulate -LR zsh
   setopt prompt_subst transient_rprompt
   local black=$fg_bold[black]
-  PS1=$'%{${fg[red]}%}%(?..Error: (%?%)\n)' # errors
-  PS1+="%{${fg[default]}%}[%{%(#~$fg[red]~$black)$FX[bold]%}"  # root or not
-  PS1+='%n%{${fg[default]}${bg[default]}$FX[reset]%} $chpwd_s_str'  # Username
-  PS1+="$(((SHLVL>1))&&echo " <"${SHLVL}">")]%#$nbsp" # shell depth
+
+  # show the last error code
+  PS1=$'%{${fg[red]}%}%(?..Error: (%?%)\n)'
+
+  # highlight root in red
+  PS1+="%{${fg[default]}%}[%{%(#~$fg[red]~$black)$FX[bold]%}"
+
+  # username and reset decorations, compressed_path
+  PS1+='%n%{${fg[default]}${bg[default]}$FX[reset]%} $chpwd_s_str'
+
+  if [[ ! -n ${MC_TMPDIR+1} ]]; then
+    # shell depth
+    PS1+="$(((SHLVL>1))&&echo " <"${SHLVL}">")"
+
+    # vim normal/textobject mode indicator
+    local VIM_PROMPT="%{$fg_bold[black]%} [% N]% %{$reset_color%}"
+    local VIM_PROMPT_OPP="%{$fg_bold[black]%} [% N+]% %{$reset_color%}"
+    RPS1="${${${KEYMAP/vicmd/$VIM_PROMPT}/opp/$VIM_PROMPT_OPP}/(afu)/}"
+    RPS1=$RPS1"\${vcs_info_msg_0_}"
+
+  else
+    # right prompt messes Midnight Commander up.
+    RPS1=""
+    PS1+=" \${vcs_info_msg_0_} "
+  fi
   
-  local VIM_PROMPT="%{$fg_bold[black]%} [% N]% %{$reset_color%}"
-  local VIM_PROMPT_OPP="%{$fg_bold[black]%} [% N+]% %{$reset_color%}"
-  RPS1="${${${KEYMAP/vicmd/$VIM_PROMPT}/opp/$VIM_PROMPT_OPP}/(afu)/}"
-  RPS1=$RPS1"\${vcs_info_msg_0_}"
+  # finish the prompt
+  PS1+="]%#$nbsp"
 }
 
 compute_prompt
