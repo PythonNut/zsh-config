693120a8c980fb383d26bebae88fbcc96320a8a6
PythonNut <PythonNut@users.noreply.github.com>
2014-09-29 20:19:38 -0700

    Only hash hostname if hash program installed

--- modules/18_prompt.zsh
+++ modules/18_prompt.zsh
@@ -21,9 +21,11 @@ function compute_prompt () {
 
   if (( $degraded_terminal[display_host] == 1 )); then
     if (( $degraded_terminal[colors256] != 1 )); then
-      # hash hostname and generate one of 256 colors
-      PS1+="%F{$((0x${$(print -P '%m'|md5sum):1:2}))}"
-      PS1+="@${${(j: :)$(print -P "%m")}:0:3}%k%f"
+      if (( $+commands[md5sum] )); then
+        # hash hostname and generate one of 256 colors
+        PS1+="%F{$((0x${$(print -P '%m'|md5sum):1:2}))}"
+        PS1+="@${${(j: :)$(print -P "%m")}:0:3}%k%f"
+      fi
     fi
   fi
 
