efcf5bf6baec925bc9a7fcef350fa3f80354d40b
PythonNut <PythonNut@users.noreply.github.com>
2014-10-07 00:13:39 +0000

    Made minify_path_fasd calls async (significant speedup!)

--- modules/22_chpwd.zsh
+++ modules/22_chpwd.zsh
@@ -6,9 +6,9 @@ TMPPREFIX=/dev/shm/ # use shared memory
 
 function chpwd_async_worker () {
   emulate -LR zsh
-  chpwd_s_str=$(minify_path_smart .)
-
-  zsh_pickle -i async-chpwd chpwd_s_str
+  chpwd_minify_smart_str=$(minify_path_smart .)
+  chpwd_minify_fasd_str=$(minify_path_fasd .)
+  zsh_pickle -i async-chpwd chpwd_minify_smart_str chpwd_minify_fasd_str
 
   # Signal the parent shell to update the prompt.
   kill -USR2 $$
@@ -21,6 +21,9 @@ function TRAPUSR2 {
 
   # Force zsh to redisplay the prompt.
   zle && zle reset-prompt
+
+  # and update the title
+  title_async_compress
 }
 
 # Build the prompt in a background job.
@@ -36,15 +39,14 @@ add-zsh-hook chpwd recompute_cdpath
 function prompt_async_compress () {
   # check if we're running under Midnight Commander
   if (( $degraded_terminal[decorations] == 1 )); then
-    chpwd_s_str=${${:-.}:A:t}
+    chpwd_minify_smart_str=${${:-.}:A:t}
     zle && zle reset-prompt
   else
-    chpwd_s_str=$(minify_path .)
-    chpwd_s_fallback_str=$chpwd_s_str
+    chpwd_minify_smart_str=$(minify_path .)
+    chpwd_minify_fast_str=$chpwd_minify_smart_str
     chpwd_async_worker &!
   fi
 }
-
 
 add-zsh-hook chpwd prompt_async_compress
 prompt_async_compress
