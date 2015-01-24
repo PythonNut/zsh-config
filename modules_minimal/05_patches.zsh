1776213c80d6dc77bf595ee7ad3515a39f48f342
PythonNut <PythonNut@users.noreply.github.com>
2015-01-24 09:50:21 -0800

    Fix indentation in minimal init patches

--- modules_minimal/05_patches.zsh
+++ modules_minimal/05_patches.zsh
@@ -1,6 +1,6 @@
 function global_bindkey () {
-    bindkey -M command $@
-    bindkey -M emacs   $@
-    bindkey -M main    $@
-    bindkey            $@
+  bindkey -M command $@
+  bindkey -M emacs   $@
+  bindkey -M main    $@
+  bindkey            $@
 }
--- modules_minimal/21_title.zsh
+++ modules_minimal/21_title.zsh
@@ -2,82 +2,82 @@ integer chpwd_title_manual
 
 # set the title
 function _settitle() {
-    emulate -LR zsh
+  emulate -LR zsh
 
-    if (( $degraded_terminal[title] == 1 )); then
-        return 0
-    fi
+  if (( $degraded_terminal[title] == 1 )); then
+    return 0
+  fi
 
-    local titlestart titlefinish
+  local titlestart titlefinish
 
-    # determine the terminals escapes
-    case "$_OLD_TERM" in
-        (xterm*)
-            titlestart='\e]0;'
-            titlefinish='\a';;
-        (aixterm|dtterm|putty|rxvt)
-            titlestart='\033]0;'
-            titlefinish='\007';;
-        (cygwin)
-            titlestart='\033];'
-            titlefinish='\007';;
-        (konsole)
-            titlestart='\033]30;'
-            titlefinish='\007';;
-        (screen*|screen)
-            titlestart='\033k'
-            titlefinish='\033\';;
-        (*)
-            if {hash tput && tput longname} &>/dev/null; then
-                titlestart="$(tput tsl)"
-                titlefinish="$(tput fsl)"
-            fi
-    esac
+  # determine the terminals escapes
+  case "$_OLD_TERM" in
+    (xterm*)
+      titlestart='\e]0;'
+      titlefinish='\a';;
+    (aixterm|dtterm|putty|rxvt)
+      titlestart='\033]0;'
+      titlefinish='\007';;
+    (cygwin)
+      titlestart='\033];'
+      titlefinish='\007';;
+    (konsole)
+      titlestart='\033]30;'
+      titlefinish='\007';;
+    (screen*|screen)
+      titlestart='\033k'
+      titlefinish='\033\';;
+    (*)
+      if {hash tput && tput longname} &>/dev/null; then
+        titlestart="$(tput tsl)"
+        titlefinish="$(tput fsl)"
+      fi
+  esac
 
-    if test -z "${titlestart}"; then
-        return 0
-    fi
+  if test -z "${titlestart}"; then
+    return 0
+  fi
 
-    print -Pn "${(%)titlestart}$* ${(%)titlefinish}"
+  print -Pn "${(%)titlestart}$* ${(%)titlefinish}"
 }
 
 # if title set manually, dont set automatically
 function settitle() {
-    emulate -LR zsh
-    chpwd_title_manual=1
-    _settitle $1
-    if [[ ! -n $1 ]]; then
-        chpwd_title_manual=0
-        _settitle
-    fi
+  emulate -LR zsh
+  chpwd_title_manual=1
+  _settitle $1
+  if [[ ! -n $1 ]]; then
+    chpwd_title_manual=0
+    _settitle
+  fi
 }
 
 function title_compress_command () {
-    if (( $degraded_terminal[title] != 1 && $chpwd_title_manual == 0 )); then
-        local cur_command
+  if (( $degraded_terminal[title] != 1 && $chpwd_title_manual == 0 )); then
+    local cur_command
 
-        local host=""
-        if (( $degraded_terminal[display_host] == 1 )); then
-            host="$(print -P '%m') "
-        fi
-
-        cur_command=${${1##[[:space:]]#}%%[[:space:]]*}
-        _settitle "${host}$(print -P '%1~') $cur_command"
+    local host=""
+    if (( $degraded_terminal[display_host] == 1 )); then
+      host="$(print -P '%m') "
     fi
+
+    cur_command=${${1##[[:space:]]#}%%[[:space:]]*}
+    _settitle "${host}$(print -P '%1~') $cur_command"
+  fi
 }
 
 add-zsh-hook preexec title_compress_command
 
 function title_compress () {
-    if (( $degraded_terminal[title] != 1 && $chpwd_title_manual == 0 )); then
-        local host=""
+  if (( $degraded_terminal[title] != 1 && $chpwd_title_manual == 0 )); then
+    local host=""
 
-        if (( $degraded_terminal[display_host] == 1 )); then
-            host="$(print -P '%m') "
-        fi
-
-        _settitle "${host}$(print -P '%1~')"
+    if (( $degraded_terminal[display_host] == 1 )); then
+      host="$(print -P '%m') "
     fi
+
+    _settitle "${host}$(print -P '%1~')"
+  fi
 }
 
 add-zsh-hook precmd title_compress
