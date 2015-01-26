1e634e7848cd1a9f49e466c12f5becd5ebcf401c
PythonNut <PythonNut@users.noreply.github.com>
2015-01-25 20:40:12 -0800

    Fix zle-line-init requirement

--- modules_minimal/05_patches.zsh
+++ modules_minimal/05_patches.zsh
@@ -4,3 +4,7 @@ function global_bindkey () {
   bindkey -M main    $@
   bindkey            $@
 }
+
+# because some other lines call this function to reset state
+zle-line-init () { }
+zle -N zle-line-init
