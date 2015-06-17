d4fa49e9a44eb12e5831923ad64c6fb71384aabe
PythonNut <PythonNut@users.noreply.github.com>
2015-06-17 17:32:56 +0000

    Add a useful zsh_directory_name handler

--- modules/05_fasd.zsh
+++ modules/05_fasd.zsh
@@ -28,3 +28,24 @@ alias sf='fasd -sif'
 
 # cd, same functionality as j in autojump
 alias j='fasd -e cd -d'
+
+_mydirstack() {
+  local -a lines list
+  for d in $dirstack; do
+    lines+="$(($#lines+1)) -- $d"
+    list+="$#lines"
+  done
+  _wanted -V directory-stack expl 'directory stack' \
+          compadd "$@" -ld lines -S']/' -Q -a list
+}
+
+zsh_directory_name() {
+  case $1 in
+    (c) _mydirstack;;
+    (n) case $2 in
+          (<0-9>) reply=($dirstack[$2]);;
+          (*) reply=($dirstack[(r)*$2*]);;
+        esac;;
+    (d) false;;
+  esac
+}
