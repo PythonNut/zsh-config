f013a28d2cf082b593dd908afdf1443eb49d78f7
PythonNut <PythonNut@users.noreply.github.com>
2015-09-21 22:22:14 -0700

    Add better mosh detection

--- modules/03_env.zsh
+++ modules/03_env.zsh
@@ -120,7 +120,7 @@ if [[ $(who am i) == *\([-a-zA-Z0-9.]##*\)(#e) ]]; then
   degraded_terminal[display_host]=1
 elif [[ -n $TMUX && -n $SSH_CLIENT ]]; then
   degraded_terminal[display_host]=1
-elif [[ $(ps -o comm= -p $PPID 2>/dev/null) == (sshd|*/sshd) ]]; then
+elif [[ $(ps -o comm= -p $PPID 2>/dev/null) == (sshd|*/sshd|mosh-server) ]]; then
   degraded_terminal[display_host]=1
 fi
 
