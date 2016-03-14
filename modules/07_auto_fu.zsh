42f40b02ddd0ab9a041efb306df5f4fe4fb70d97
PythonNut <PythonNut@users.noreply.github.com>
2016-03-14 00:08:24 +0000

    Add autosuggestions for full config

--- modules/07_auto_fu.zsh
+++ modules/07_auto_fu.zsh
@@ -61,6 +61,12 @@
   setopt rc_expand_param
 } &>> $ZDOTDIR/startup.log
 
+{
+  source $ZDOTDIR/zsh-async/async.zsh
+  source $ZDOTDIR/zsh-autosuggestions/zsh-autosuggestions.zsh
+  ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=("expand-or-complete" "pcomplete")
+} &>> $ZDOTDIR/startup.log
+
 function global_bindkey () {
   bindkey -M command $@
   bindkey -M emacs   $@
