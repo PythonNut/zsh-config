ad81940d350702664f57cf1e646154806d4ef7b6
PythonNut <PythonNut@users.noreply.github.com>
2017-01-13 00:35:26 +0000

    Lazy-load plugins when possible

--- modules/01_zplug.zsh
+++ modules/01_zplug.zsh
@@ -11,15 +11,15 @@ zplug "zsh-users/zsh-syntax-highlighting", at:feec9f5
 zplug "zsh-users/zsh-completions"
 zplug "yonchu/zsh-vcs-prompt"
 zplug "seebi/dircolors-solarized"
-zplug "clvv/fasd", as:command
+zplug "clvv/fasd", as:command, lazy:true
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zaw"
-zplug "yonchu/zaw-src-git-log", on:"zsh-users/zaw"
-zplug "yonchu/zaw-src-git-show-branch", on:"zsh-users/zaw"
+zplug "yonchu/zaw-src-git-log", on:"zsh-users/zaw", lazy:true
+zplug "yonchu/zaw-src-git-show-branch", on:"zsh-users/zaw", lazy:true
 zplug "mafredri/zsh-async"
 zplug "PythonNut/zsh-autosuggestions", on:"zsh-async"
 zplug "knu/zsh-git-escape-magic"
-zplug "coldfix/zsh-soft-history"
+zplug "coldfix/zsh-soft-history", lazy:true
 zplug "hchbaw/auto-fu.zsh", at:pu, on:"zsh-users/zsh-syntax-highlighting"
 
 if ! zplug check; then
