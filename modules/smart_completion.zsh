bf92b5656e535f25320eb9ece9ca087a81570238
PythonNut <PythonNut@users.noreply.github.com>
2017-06-17 20:24:05 -0700

    Reduce side-effects in smart completion

--- modules/smart_completion.zsh
+++ modules/smart_completion.zsh
@@ -8,6 +8,18 @@ function pcomplete() {
     setopt function_argzero prompt_subst extended_glob
     setopt list_packed list_rows_first
 
+    local -a show_completer
+    local -a extra_verbose
+    local -a verbose
+    local -a completer
+    local -a menu
+
+    zstyle -a ':completion:*' show-completer show_completer
+    zstyle -a ':completion:*' extra-verbose extra_verbose
+    zstyle -a ':completion:*' verbose verbose
+    zstyle -a ':completion:*' completer completer
+    zstyle -a ':completion:*' menu menu
+
     setopt auto_list              # list if multiple matches
     setopt complete_in_word       # complete at cursor
     setopt menu_complete          # add first of multiple
@@ -28,7 +40,6 @@ function pcomplete() {
     zstyle ':completion:*' show-completer true
     zstyle ':completion:*' extra-verbose true
     zstyle ':completion:*' verbose true
-    zstyle ':completion:*' insert-unambiguous true
     zstyle ':completion:*' menu select=1 interactive
 
     zstyle ':completion:*' completer \
@@ -65,11 +76,11 @@ function pcomplete() {
       zle menu-expand-or-complete
     fi
 
-    zstyle ':completion:*' show-completer false
-    zstyle ':completion:*' extra-verbose false
-    zstyle ':completion:*' verbose false
-    zstyle ':completion:*' completer _oldlist _complete
-    zstyle ':completion:*' menu yes no-select
+    zstyle ':completion:*' show-completer $show_completer
+    zstyle ':completion:*' extra-verbose $extra_verbose
+    zstyle ':completion:*' verbose $verbose
+    zstyle ':completion:*' completer $completer
+    zstyle ':completion:*' menu $menu
 
   } always {
     unfunction "pcomplete_forward_word"
